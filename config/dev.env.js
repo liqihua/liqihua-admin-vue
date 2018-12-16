f3dc15bbebb7fd683eb5e7bee76433344128cfd2
liqihua <iamliqihua@qq.com>
2018-12-16 19:48:38 +0800

    liqihua

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://127.0.0.1:9001/liqihua"',
+  BASE_API: '"http://localhost:9001/liqihua"',
 })
