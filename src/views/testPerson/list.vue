fffd2a0443539e7b6c5467a001f8e4f29c0f7e3a
liqihua <iamliqihua@qq.com>
2018-12-27 16:25:49 +0800

    liqihua

--- src/views/testPerson/list.vue
+++ src/views/testPerson/list.vue
@@ -62,7 +62,7 @@ export default {
                 this.total = response.data.total
                 this.loading = false
             }).catch(error => {
-                    console.log(error)
+                console.log(error)
                 this.loading = false
             })
         },
